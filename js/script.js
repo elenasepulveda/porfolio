19c6a62874a3cd8ca6302ebd46ce255a0804a44e
elenasepulveda <1elenasepulveda@gmail.com>
2024-06-13 20:22:44 +0200

    nuevos cambios 20:22

--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
 window.addEventListener('scroll', function() {
-    const scrollPosition = window.scrollY;
-    document.body.style.backgroundPositionY = `${scrollPosition * 0.5}px`;
+    let scrollPosition = window.scrollY;
+    document.body.style.backgroundPosition = `0 ${scrollPosition * 0.5}px`; // Ajusta el factor multiplicador para modificar la velocidad del desplazamiento
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    const typedTextElement = document.getElementById('typed-text');
+    const textArray = ["Bienvenid@ a mi porfolio.", "Soy Software Developer."];
+    const typingDelay = 100;
+    const erasingDelay = 60;
+    const newTextDelay = 2000; // Delay between current and next text
+    let textArrayIndex = 0;
+    let charIndex = 0;
+
+    function type() {
+        if (charIndex < textArray[textArrayIndex].length) {
+            typedTextElement.textContent += textArray[textArrayIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(type, typingDelay);
+        } else {
+            setTimeout(erase, newTextDelay);
+        }
+    }
+
+    function erase() {
+        if (charIndex > 0) {
+            typedTextElement.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+            charIndex--;
+            setTimeout(erase, erasingDelay);
+        } else {
+            textArrayIndex++;
+            if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+            setTimeout(type, typingDelay + 1100);
+        }
+    }
+
+    setTimeout(type, newTextDelay + 250);
+});
