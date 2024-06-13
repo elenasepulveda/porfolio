daccc4a76efa8f7276c496629db3bdf6bcdb8b01
elenasepulveda <1elenasepulveda@gmail.com>
2024-06-13 13:47:38 +0200

    cambios

--- js/script.js
+++ js/script.js
@@ -3,12 +3,3 @@ window.addEventListener('scroll', function() {
     document.body.style.backgroundPositionY = `${scrollPosition * 0.5}px`;
 });
 
-window.addEventListener('scroll', function() {
-    const nav = document.querySelector('nav');
-    if (window.scrollY > 50) {
-        nav.classList.add('scrolled');
-    } else {
-        nav.classList.remove('scrolled');
-    }
-});
-
