d16edd4340400151d20273b3b569d2344147d699
elenasepulveda <1elenasepulveda@gmail.com>
2024-06-13 13:46:06 +0200

    actualizacion3

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ window.addEventListener('scroll', function() {
     const scrollPosition = window.scrollY;
     document.body.style.backgroundPositionY = `${scrollPosition * 0.5}px`;
 });
+
+window.addEventListener('scroll', function() {
+    const nav = document.querySelector('nav');
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
+
